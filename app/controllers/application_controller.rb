d6a3d24c34b3f34196881beef5780dcd9e2599fd
Adam Griffis <adam.griffis@hannonhill.com>
2012-03-18 15:41:37 -0400

    Added shopping cart.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+    
+    def current_cart
+      Cart.find(session[:card_id])
+    rescue ActiveRecord::RecordNotFound
+        cart = Cart.create
+        session[:card_id] = cart.id
+        cart
+    end
+    
+    def increment_count
+      if session[:visit_count].nil? then
+        session[:visit_count] = 0
+      else
+        session[:visit_count] += 1
+      end
+      
+      session[:visit_count]
+    end
+
+    def reset_count
+      session[:visit_count] = 0
+    end
 end
